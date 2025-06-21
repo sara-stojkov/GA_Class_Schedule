bccfcea3661063528545b2644715e5aa3050b611
Sara Stojkov <sarastojkov6@gmail.com>
2025-06-21 19:50:03 +0200

    introduced blocks to stop overlap, now changing fitness and params

--- const.py
+++ const.py
@@ -1,9 +1,9 @@
 DAYS = ["Ponedeljak", "Utorak", "Sreda", "Četvrtak", "Petak", "Subota", "Nedelja"] # day names for display
 DAYS_NUM = 5
 FILE_PATH = "GA_Class_Schedule\data_timetable.txt"
-POPULATION_SIZE = 200
-MUTATION_CHANCE = 0.02
+POPULATION_SIZE = 100
+MUTATION_CHANCE = 0.1
 KEEP_PERCENT = 0.3
-MAX_GENERATIONS = 1000
+MAX_GENERATIONS = 10000
 
 
--- genetic_algorithm/individual.py
+++ genetic_algorithm/individual.py
@@ -3,19 +3,19 @@ from random import random, randint
 
 from structures.subject import Subject
 
-
 class Schedule:
     """
     Represents a schedule for classes in a school. It contains class_list, 
     it is a list of lists (each sublist represents a time slot so there are 12 hours * 4 quarters * 5 days  * room_count),
     so the first list is on Monday at 7:00 in the first room, the second is on Monday at 7:15 also first room, and so on.
     If the class lasts 1 hour, it will occupy 4 consecutive slots.
+    Also, blocks are introduced to prevent overlap of one subject across days or different rooms.
     It is a list of lists, because in the beginning we allow multiple classes to be scheduled at the same time,
     but this will be corrected later in the algorithm (negative impact on fitness score).
     The mapping is a dictionary that maps class indices to their first positions (start of the class) in the class_list.
 
     """
-    __slots__ = ('class_list', 'mapping', 'fitness_score')
+    __slots__ = ('class_list', 'mapping', 'fitness_score', 'num_blocks', 'block_size')
     def __init__(self, class_count: int, room_count: int):
         # Initialize the schedule where the time slots are represented as a list of lists.
         # Each sublist corresponds to a time slot (12 hours * 4 quarters * 5 days * room_count).
@@ -25,6 +25,8 @@ class Schedule:
         # The mapping will be updated when classes are added to the schedule.
         self.mapping = {i: -1 for i in range(class_count)}
         self.fitness_score = -1
+        self.num_blocks = 5 * room_count  # We have 5 work days and n rooms, so each combination of these is one block
+        self.block_size = 4 * 12 # Since we work with quarters of an hour, this is 4 quarters per 12 hours (from 7 am to 7 pm)
 
     def get_class_list(self):
         """Returns the list of classes in the schedule."""
@@ -49,7 +51,12 @@ class Schedule:
         Adding a class to the schedule is done by appending it to the class_list and updating the mapping.
         """
         for i in range(class_number): # iterating through the indexes of all classes
-            random_class_index = randint(0, len(self.class_list) - classes[i].duration)
+            block_id = randint(0, self.num_blocks - 1) # first pick out a block
+            block_start = block_id * self.block_size
+            block_end = block_start + self.block_size - classes[i].duration
+
+            random_class_index = randint(block_start, block_end)
+
             for j in range(classes[i].duration):
                     self.class_list[random_class_index + j].append(i) 
             self.mapping[i] = random_class_index
@@ -84,59 +91,62 @@ class Schedule:
 
         first_part_score = 0
         second_part_score = 0
-        max_point = 3
+        max_point = 20
 
         for k, v in self.mapping.items():
             curr = 0
             # if there is no class right before
             duration = all_class[k].get_duration()
             if v == 0:
-                curr+=1
+                curr+=5
             else:
                 if self.class_list[v-1] == []:
-                    curr+=1
+                    curr+=5
             # if there is no class right after
             if v + duration == len(self.class_list):
-                curr+=1
+                curr+=5
             elif self.class_list[v + duration] == []:
-                curr+=1
+                curr+=5
             # if there is no class in the same time slot
-            temp_point = 1
+            temp_point = 10
             for i in range(v, v + duration):
                 if len(self.class_list[i]) > 1:
-                    temp_point = 0
+                    temp_point = - 50
                     break
 
             first_part_score += curr + temp_point
 
-        first_part_score = first_part_score / (len(self.mapping) * max_point)  * 1000000
+        first_part_score = (first_part_score / (len(self.mapping) * max_point)) * 10000000
  
 
         for i in range(0, len(self.class_list),12*4):
-            sufix=0
-            prefix=0
+            sufix=-1
+            prefix=-1
             # Find the first non-empty slot in the first part of the day, start of the working day
             for j in range(12*4):
                 if self.class_list[i+j]:
                     prefix = j
                     break
+            if prefix==-1:
+                prefix=sufix=12*4
+                
             # Find the last non-empty slot in the first part of the day, end of the working day
             for j in range(12*4-1, -1, -1):
                 if self.class_list[i+j]:
                     sufix = j
                     break
-            second_part_score += (prefix**2)*15 * (sufix**2) *15
+            second_part_score += (prefix**2) * (sufix**2)
                 
-        fitness_score = first_part_score + second_part_score
+        fitness_score = first_part_score * second_part_score
         self.set_fitness_score(fitness_score)
         return
     
     # In the future possibly change more than 2 classes
     def mutate(self, all_clases: list[Subject]):
         """Funtion that handles the mutation of an individual.
-           To improve variability, there will be 3 kinds of mutations:
+           To improve variability, there will be 2 kinds of mutations:
            1. Moving a random class from scheduled classes to other random time slot.
-           2. Switching two classes time slots.
+           2. Switching two classes time slots. (In the works, causes errors)
            """
         option = random()
         if option > 0.5:
@@ -150,7 +160,11 @@ class Schedule:
                 if class_index in self.class_list[i]:
                     self.class_list[i].remove(class_index)
 
-            new_position = randint(0, len(self.class_list) - duration) # We choose a spot in the Schedule (all days, rooms) to which we move the class
+            block_id = randint(0, self.num_blocks - 1) # We choose a new spot in the schedule by picking out a block and then randomizing
+            block_start = block_id * self.block_size
+            block_end = block_start + self.block_size - duration
+
+            new_position = randint(block_start, block_end) 
 
             for j in range(new_position, new_position + duration):
                 self.class_list[j].append(class_index)
